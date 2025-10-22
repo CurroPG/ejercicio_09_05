ebecafe4bd32d361df8f815ac43cdc0a8af3a166
Curro Portillo <211curroportillo@gmail.com>
2025-10-22 21:04:06 +0200

    ejercicio 9

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Este programa cuenta los dígitos de un número.");
+        int numero = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+        int aux = numero;
+        int digito = 0;
+        do{
+            aux/=10;
+            digito++;
+        }while(aux!=0);
+
+        System.out.printf("%d tiene %d dígito/s.", numero, digito);
     }
 }
